7a5d3699753573e5f3e312a92e74e55eb6a8f927
wertkh32 <virtwert@hotmail.com>
2015-03-04 12:26:26 -0500

    spatial hash start

---
+++ SpatialHash.cpp
@@ -0,0 +1,11 @@
+#include "SpatialHash.h"
+
+
+SpatialHash::SpatialHash(void)
+{
+}
+
+
+SpatialHash::~SpatialHash(void)
+{
+}
---
+++ SpatialHash.h
@@ -0,0 +1,14 @@
+#pragma once
+#define OBJECTS_PER_CELL 10
+#define CELL_SIZE 3
+#define DIM_X 100
+#define DIM_Y 30
+#define DIM_Z 100
+
+class SpatialHash
+{
+public:
+	SpatialHash(void);
+	~SpatialHash(void);
+};
+
